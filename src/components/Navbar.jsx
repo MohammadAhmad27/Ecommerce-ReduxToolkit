86bb39d03ec33289a1be7d2fee44489557add448
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-07-04 15:03:28 +0500

    Added Navbar Component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div
+      className="fixed-top bg-dark"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "1rem"
+      }}
+    >
+      <span style={{ color: "white", fontWeight: "400" }}>Redux Toolkit</span>
+      <div>
+        <Link
+          to="/"
+          style={{ fontWeight: "bolder", marginRight: "5px", color: "white" }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/cart"
+          style={{ fontWeight: "bolder", marginRight: "10px", color: "white" }}
+        >
+          Cart
+        </Link>
+        <span style={{ color: "white", fontWeight: "400" }}>
+          items: <span style={{ fontWeight: "bolder" }}>0</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
