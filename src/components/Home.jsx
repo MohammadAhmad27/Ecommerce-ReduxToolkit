2da3bb33ee10cc5c97d6b9901429d8d1aafde54a
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-07-04 18:59:14 +0500

    Added code to fetch data and add items to /cart route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../redux/CartSlice";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -10,23 +13,39 @@ const Home = () => {
       setProducts(data);
     };
     fetchProduct();
+    
   }, []);
+
+  const handleAdd = (product) => {
+    dispatch(add(product));
+  };
+
 
   return (
     <div>
-        <div style={{marginBottom: "70px"}}></div>
-      <div className="row">
+      <div style={{ marginBottom: "70px" }}></div>
+      <div className="row offset-2">
         {products.map((product) => (
           <div
-            className="row card mx-2 mb-2"
+            className="row card mx-2 mb-4 col-lg-3 col-md-4 col-sm-6"
             key={product.id}
-            style={{width: "18rem"}}
+            style={{ width: "18rem" }}
           >
-            <img src={product.image} alt="img" width={"200px"} />
+            <img
+              src={product.image}
+              alt="img"
+              width={"150px"}
+              height={"150px"}
+            />
             <div className="card-body">
               <h4>{product.title}</h4>
               <h5>{product.price}</h5>
-              <button className="btn btn-danger">Add to Cart</button>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleAdd(product)}
+              >
+                Add to Cart
+              </button>
             </div>
           </div>
         ))}
