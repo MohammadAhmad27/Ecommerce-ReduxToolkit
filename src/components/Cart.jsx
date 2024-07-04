9cbfad0c6497829378d76597c0a6f2dccd7c4cbc
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-07-04 18:57:08 +0500

    Added code to display Cart Items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../redux/CartSlice";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartItems = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-export default Cart
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
+
+  return (
+    <div>
+      <div style={{ marginBottom: "50px" }}></div>
+      <h1 style={{ textAlign: "center", marginBottom: "17px" }}>Cart Items</h1>
+      <div className="row offset-2">
+        {cartItems.map((item) => (
+          <div
+            className="row card mx-2 mb-4 col-lg-3 col-md-4 col-sm-6"
+            key={item.id}
+            style={{ width: "18rem" }}
+          >
+            <img src={item.image} alt="img" width={"150px"} height={"150px"} />
+            <div className="card-body">
+              <h4>{item.title}</h4>
+              <h5>{item.price}</h5>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleRemove(item.id)}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
